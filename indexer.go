6f126b0de213312c626aa4bc24340fcee931116b
Pablo Ocampo <pablojocampogo@gmail.com>
2022-05-03 15:37:18 -0400

    feat: add comments to structs and interfaces

--- indexer.go
+++ indexer.go
@@ -1,19 +1,23 @@
 package indexer
 
+// Provider interface of needed provider functions
 type Provider interface {
 	GetBlockHeight() (int, error)
 	// TODO: add missing functions
 }
 
+// Database interface of needed database functions
 type Database interface {
 	// TODO: add missing functions
 }
 
+// Indexer struc handler for Indexer functions
 type Indexer struct {
 	provider Provider
 	database Database
 }
 
+// NewIndexer returns Indexer instance with given input
 func NewIndexer(provider Provider, database Database) *Indexer {
 	return &Indexer{
 		provider: provider,
@@ -21,6 +25,7 @@ func NewIndexer(provider Provider, database Database) *Indexer {
 	}
 }
 
+// IndexBlock transfers all transactions to a known strcuture and saves them
 func (i *Indexer) IndexBlock(block int) error {
 	return nil
 }
