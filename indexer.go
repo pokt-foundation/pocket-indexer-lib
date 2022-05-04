4e64a780485c6bfe44d24fa075424d63e8d8d05e
crisog <ortega.cpp@gmail.com>
2022-05-04 14:01:14 -0400

    feat: persistance interface

--- indexer.go
+++ indexer.go
@@ -1,31 +1,46 @@
 package indexer
 
+import (
+	"github.com/pokt-foundation/pocket-indexer/types"
+)
+
 // Provider interface of needed provider functions
 type Provider interface {
 	GetBlockHeight() (int, error)
 	// TODO: add missing functions
 }
 
-// Writer interface of write function
-type Writer interface {
-	// TODO: add missing functions
+// Persistance layer interface (database, in-memory, etc.)
+type Persistance interface {
+	// Writes
+	WriteBlock(block *types.Block) error
+	WriteTransactions(txs []*types.Transaction) error
+	// Reads
+	ReadBlock(blockHeight int) (*types.Block, error)
+	ReadBlockTransactions(blockHeight int) ([]*types.Transaction, error)
+	ReadTransaction(hash string) (*types.Transaction, error)
 }
 
 // Indexer struc handler for Indexer functions
 type Indexer struct {
-	provider Provider
-	writer   Writer
+	provider    Provider
+	persistance Persistance
 }
 
 // NewIndexer returns Indexer instance with given input
-func NewIndexer(provider Provider, writer Writer) *Indexer {
+func NewIndexer(provider Provider, persistance Persistance) *Indexer {
 	return &Indexer{
-		provider: provider,
-		writer:   writer,
+		provider:    provider,
+		persistance: persistance,
 	}
 }
 
-// IndexBlock transfers all transactions to a known strcuture and saves them
+// IndexBlock converts block details to a known structure and saves them
 func (i *Indexer) IndexBlock(blockHeight int) error {
+	return nil
+}
+
+// IndexBlock converts block transactions to a known structure and saves them
+func (i *Indexer) IndexBlockTransactions(blockHeight int) error {
 	return nil
 }
